83ca6bfdeccc5c9884eeeab14cd5073ea822f8c0
sumit-sharma <sharmasumit8741@gmail.com>
2023-04-10 12:54:32 +0530

    form validation

--- js/script.js
+++ js/script.js
@@ -32,3 +32,34 @@ $(document).ready(function () {
 
     });
 });
+function validateData() {
+    var userName = document.getElementById('userName').value;
+    var userEmail = document.getElementById('userEmail').value;
+    var userMessage = document.getElementById('userMessage').value;
+    console.log(userName, userEmail, userMessage);
+    var regName = /^[a-zA-Z]+ [a-zA-Z]+$/;
+    var regMail = /^a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,63}$/;
+    var regMessage = /^[A-Za-z]+$/;
+    if (userName.length == 0 || userEmail.length == 0 || userMessage.length == 0) {
+        event.preventDefault();
+        $(".error_message").show();
+        $(".error_message").html("Please fill all the details.");
+    } else if (!regName.test(userName) || userEmail.match(regMail) || userMessage.match(regMessage)) {
+        event.preventDefault();
+        $(".error_message").show();
+        $(".error_message").html("Please fill valid details.");
+    } else {
+        $(".error_message").hide();
+        // Email.send({
+        //     Host: "smtp.elasticemail.com",
+        //     Username: 'sumitsharma8741@gmail.com',
+        //     Password: "password",
+        //     To: 'sharmasumit8741@gmail.com',
+        //     From: 'sumitsharma8741@gmail.com',
+        //     Subject: "This is the subject",
+        //     Body: "And this is the body"
+        // }).then(
+        //     message => alert("mail send ")
+        // );
+    }
+}
